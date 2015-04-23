b3c31a00dbf10a7cc540ffcd817c866929e5bd7a
1000000000 <lthyde@gmail.com>
2015-04-23 11:37:11 -0400

    Finished program

--- src/main/java/net/clonecomputers/lab/ntina/SubgroupLTester.java
+++ src/main/java/net/clonecomputers/lab/ntina/SubgroupLTester.java
@@ -1,6 +1,12 @@
 package net.clonecomputers.lab.ntina;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 public class SubgroupLTester implements Runnable {
 	
@@ -17,19 +23,58 @@ public class SubgroupLTester implements Runnable {
 	@Override
 	public void run() {
 		long tuple = 1;
+		int power = 1;
+		File outputFile = new File("data.csv");
+		int a = 0;
+		while(outputFile.exists()) {
+			outputFile = new File("out-" + ++a + ".csv");
+		}
+		CSVPrinter p = null;
+		try {
+			p = new CSVPrinter(new BufferedWriter(new FileWriter(outputFile)), CSVFormat.RFC4180.withRecordSeparator('\n'));
+		} catch (IOException e) {
+			System.err.println("Exception opening writing to " + outputFile);
+			e.printStackTrace();
+			return;
+		}
+		try {
 		while (running) {
-			//TODO do stuff here
-			if (tuple == Long.MAX_VALUE) break;
+			if (tuple >= 1 << power) {
+				p.println();
+				System.out.println(power);
+				++power;
+			}
+			long lValue = getLValue(tuple);
+			p.print(lValue);
+			if (tuple == Long.MAX_VALUE) {
+				System.out.println("Stopping due to long overflow");
+				break;
+			}
 			++tuple;
+		}
+		p.flush();
+		} catch (IOException e) {
+			System.err.println("Exception writing data to file");
+			e.printStackTrace();
+		} finally {
+			try {
+				p.close();
+			} catch (IOException e) {
+				System.err.println("Exception closing CSVPrinter!!!");
+				e.printStackTrace();
+			}
 		}
 	}
 	
-	public long getLValue(long tuple) {
+	public static long getLValue(long tuple) throws IllegalArgumentException {
+		if (tuple <= 0) throw new IllegalArgumentException("Tuple must be a positive integer");
 		long value = 0;
 		int power = 0;
+		int sign = 1;
 		while (tuple > 0) {
 			if (tuple % 2 != 0) {
-				value += 1L << power;
+				value += sign << power;
+				sign *= -1;
 			}
 			tuple = tuple >> 1;
 			++power;
--- src/test/java/net/clonecomputers/lab/ntina/SubgroupLTesterTest.java
+++ src/test/java/net/clonecomputers/lab/ntina/SubgroupLTesterTest.java
@@ -1,14 +1,26 @@
 package net.clonecomputers.lab.ntina;
 
 import static org.junit.Assert.*;
+import static net.clonecomputers.lab.ntina.SubgroupLTester.getLValue;
 
 import org.junit.Test;
 
 public class SubgroupLTesterTest {
 
 	@Test
-	public void test() {
-		// TODO test something
+	public void testGetLValue() {
+		assertEquals(1, getLValue(1));
+		assertEquals(-5, getLValue(0xF));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetLValueZeroExecption() {
+		getLValue(0);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetLValueNegExecption() {
+		getLValue(-1);
 	}
 
 }
